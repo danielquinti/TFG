1d8dd045d8461b60ef49204f0a7a85b3912dfb70
danielquintillan <daniel.quintillan@udc.es>
2021-09-11 21:29:36 +0200

    first approach working

--- src/lstm.py
+++ src/lstm.py
@@ -2,41 +2,57 @@
 
 import os
 import numpy as np
-os.environ["TF_CPP_MIN_LOG_LEVEL"]="2"
-
+import IPython
+import IPython.display
+import pandas as pd
+from src.WindowGenerator import *
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+
 
 N=6
 input_size=12
 output_size=12
 seq_length=N-1
 batch_size=64
-epochs=5
+MAX_EPOCHS = 20
+
+def compile_and_fit(model, window, patience=2):
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                                    patience=patience,
+                                                    mode='min')
+
+    model.compile(loss=tf.losses.MeanSquaredError(),
+                optimizer=tf.optimizers.Adam(),
+                metrics=[tf.metrics.MeanAbsoluteError()])
+
+    history = model.fit(window.train, epochs=MAX_EPOCHS,
+                      validation_data=window.val,
+                      callbacks=[early_stopping])
+    return history
+
 os.chdir("data")
-train=np.loadtxt("train.csv",delimiter=",")
-test=np.loadtxt("test.csv",delimiter=",")
-train_mask = np.ones(train.shape[0], dtype=bool)
-test_mask = np.ones(test.shape[0], dtype=bool)
-train_mask[6::6] = 0
-test_mask[6::6] = 0
-x_train=train[train_mask,:]
-x_test=test[test_mask,:]
-train_mask=~np.array(train_mask)
-test_mask=~np.array(test_mask)
-y_train=train[train_mask,:]
-y_test=test[test_mask,:]
+train=pd.read_csv("train.csv")
+val=pd.read_csv("dev.csv")
+test=pd.read_csv("test.csv")
 
-model=keras.models.Sequential()
-model.add(keras.Input(shape=(input_size,seq_length)))
-model.add(layers.SimpleRNN(128,activation='relu'))
-model.add(layers.Dense(output_size))
-print(model.summary())
-
-loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-optim=keras.optimizers.Adam(lr=0.001)
-metrics=["accuracy"]
-model.compile(loss=loss,optimizer=optim,metrics=metrics)
-model.fit(x_train,y_train,batch_size=batch_size,epochs=epochs,verbose=2)
-model.evaluate(x_test,y_test,batch_size=batch_size,verbose=2)
+conv_window = WindowGenerator(5, 1, 1,train,val,test)
+multi_step_dense = tf.keras.Sequential([
+    # Shape: (time, features) => (time*features)
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(units=12, activation='relu'),
+    tf.keras.layers.Dense(units=12, activation='relu'),
+    tf.keras.layers.Dense(units=12),
+    # Add back the time dimension.
+    # Shape: (outputs) => (1, outputs)
+    tf.keras.layers.Reshape([1, -1]),
+])
+history = compile_and_fit(multi_step_dense, conv_window)
+# IPython.display.clear_output()
+# val_performance = dict()
+# performance = dict()
+# val_performance['Multi step dense'] = multi_step_dense.evaluate(conv_window.val)
+# performance['Multi step dense'] = multi_step_dense.evaluate(conv_window.test, verbose=0)
+# print(val_performance)
+# print(performance)
