1857c2b2a7eaad62631fa60689740b509b523b9d
danielquintillan <daniel.quintillan@udc.es>
2021-11-22 22:04:25 +0100

    compare reg vs noreg for 50 window size

--- src/model/available_models.py
+++ src/model/available_models.py
@@ -1,4 +1,5 @@
 import tensorflow.keras as keras
+from keras import regularizers
 from tensorflow.keras import layers
 
 
@@ -6,6 +7,23 @@ def lstm_model(n_classes, d_classes, input_beats, label_beats):
     inputs = layers.Input((input_beats, n_classes))
     x = layers.LSTM(65, activation='relu', input_shape=(input_beats, n_classes))(inputs)
     output1 = layers.Dense(n_classes, activation='softmax', name='notes')(x)
+    output2 = layers.Dense(d_classes, activation='softmax', name='duration')(x)
+    return keras.Model(inputs=inputs, outputs=[output1, output2])
+
+def lstm_reg(n_classes, d_classes, input_beats, label_beats):
+    inputs = layers.Input((input_beats, n_classes))
+    x = layers.LSTM(
+        65,
+        activation='relu',
+        input_shape=(input_beats, n_classes),
+        kernel_regularizer=regularizers.l2(5e-4)
+    )(inputs)
+    output1 = layers.Dense(
+        n_classes,
+        activation='softmax',
+        name='notes',
+        kernel_regularizer=regularizers.l2(5e-4)
+    )(x)
     output2 = layers.Dense(d_classes, activation='softmax', name='duration')(x)
     return keras.Model(inputs=inputs, outputs=[output1, output2])
 
@@ -23,5 +41,6 @@ def ffwd_model(n_classes, d_classes, input_beats, label_beats):
 
 available_models = {
     "ffwd": ffwd_model,
-    "lstm": lstm_model
+    "lstm": lstm_model,
+    "lr": lstm_reg,
 }
