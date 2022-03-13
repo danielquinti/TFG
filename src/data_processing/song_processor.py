f67b8e6a6d7084cadaa473d206d7d12df349cb0e
danie <danielquintillan@udc.es>
2022-03-13 22:38:04 +0100

    add track insight

--- src/data_processing/song_processor.py
+++ src/data_processing/song_processor.py
@@ -5,6 +5,7 @@ import math
 import os
 import random
 import re
+import shutil
 from collections import defaultdict
 from collections.abc import Callable
 from typing import Any
@@ -13,6 +14,89 @@ import numpy as np
 import libGPFile
 import PyGuitarPro.guitarpro as gp
 import utils
+
+
+def clean_tabs():
+    def dump_merged_files(files_dict):
+        os.makedirs(os.path.join("data", "clean_tabs", "gp3"))
+        os.makedirs(os.path.join("data", "clean_tabs", "gp4"))
+        os.makedirs(os.path.join("data", "clean_tabs", "gp5"))
+        os.makedirs(os.path.join("data", "clean_tabs", "gtp"))
+
+        for idx, full_path in enumerate(files_dict.values()):
+            # print(idx)
+            shutil.copy(
+                full_path,
+                os.path.join(
+                    "data",
+                    "clean_tabs",
+                    os.path.splitext(os.path.basename(full_path))[1][1:],
+                    os.path.basename(full_path)
+                )
+            )
+
+    def get_unique_file_dict(path_list):
+        def is_acceptable(filepath, extension):
+            if extension not in ('.gp3', '.gp4', '.gp5', ".gtp") \
+                    or os.path.getsize(filepath) <= 1024:
+                return False
+            return True
+
+        files_dict = {}
+        for filepath in path_list:
+            # avoid duplicates
+            name = os.path.basename(filepath)
+            key, extension = os.path.splitext(name)
+            if not is_acceptable(filepath, extension):
+                continue
+            if not files_dict.get(key):
+                files_dict[key] = filepath
+        return files_dict
+
+    filepaths_1 = utils.get_file_paths(
+        os.path.join(
+            "data",
+            "60000Tabs"
+        )
+    )
+    filepaths_2 = utils.get_file_paths(
+        os.path.join(
+            "data",
+            "Guitar_Pro_Tabs"
+        )
+    )
+    print("Files in folder 1: {}".format(len(filepaths_1)))
+    print("Files in folder 2: {}".format(len(filepaths_2)))
+    files_dict_1 = get_unique_file_dict(filepaths_1)
+    files_dict_2 = get_unique_file_dict(filepaths_2)
+    print("Unique files in folder 1: {}".format(len(files_dict_1)))
+    print("Unique files in folder 2: {}".format(len(files_dict_2)))
+    intersection = set(files_dict_1.keys()).intersection(set(files_dict_2.keys()))
+    print("Intersection: {}".format(len(intersection)))
+    union = set(files_dict_1.keys()).union(set(files_dict_2.keys()))
+    print("Union: {}".format(len(union)))
+    difference = set(files_dict_1.keys()).difference(set(files_dict_2.keys()))
+    print("Difference: {}".format(len(difference)))
+
+    files_dict_merged = files_dict_1.copy()
+    for key, value in files_dict_2.items():
+        if key not in files_dict_merged:
+            files_dict_merged[key] = value
+
+    print("Unique files after merging: {}".format(len(files_dict_merged)))
+
+    dump_merged_files(files_dict_merged)
+
+
+def select_juicy_tracks():
+    with open("track_names.csv", 'r') as f:
+        reader = csv.reader(f)
+        mydict = {rows[0]: rows[1] for rows in reader}
+
+    total_guitar = sum(int(freq) for name, freq in mydict.items() if 'guitar' in name.lower())
+    total_piano = sum(int(freq) for name, freq in mydict.items() if 'piano' in name.lower())
+    total_bass = sum(int(freq) for name, freq in mydict.items() if 'bass' in name.lower())
+    print("Total guitar:", total_guitar, "Total piano:", total_piano, "Total bass:", total_bass)
 
 
 def shuffle_and_split_files(file_paths, split_rate):
@@ -101,19 +185,44 @@ class SongProcessor:
             split_rate: float
     ):
         self.input_path = in_path
+        self.file_paths = utils.get_file_paths(self.input_path)
         self.output_path = out_path
         self.rest_thr = rest_thr
         self.beat_thr = beat_thr
         self.track_name = wanted_track_name
         self.split_rate = split_rate
 
+    def get_track_insight(self):
+        losses = 0
+        wins = 0
+        tracks = defaultdict(lambda: 0)
+        for idx, song_path in enumerate(self.file_paths):
+            if idx >= 1000:
+                break
+            song = self.safe_read_song(song_path)
+            if song is None:
+                losses += 1
+                continue
+            for track in song.tracks:
+                key = track.name.lower()
+                if key not in tracks:
+                    tracks[key] = 1
+                else:
+                    tracks[key] += 1
+
+            print("Losses:", losses, "Wins:", wins, " Attempts:", idx, " Tracks:", len(tracks))
+            wins += 1
+
+        with open('track_names.csv', 'w') as f:
+            for key in tracks.keys():
+                f.write("%s, %s\n" % (key, tracks[key]))
+
     def process_songs(self):
-        file_paths = utils.get_file_paths(self.input_path)
-        dist_paths = shuffle_and_split_files(file_paths, self.split_rate)
+        dist_paths = shuffle_and_split_files(self.file_paths, self.split_rate)
         prepare_dirs(self.output_path)
-        for dist_name, file_paths in dist_paths.items():
-            for i, filepath in enumerate(file_paths):
-                print(f'Split attempt: {i + 1}/{len(file_paths)}')
+        for dist_name, self.file_paths in dist_paths.items():
+            for i, filepath in enumerate(self.file_paths):
+                print(f'Split attempt: {i + 1}/{len(self.file_paths)}')
                 chunks = self.process_song(filepath, dist_name)
                 song_name = self.input_path.split("\\")[-1].split(".")[0]
                 if chunks:
@@ -134,7 +243,7 @@ class SongProcessor:
         return not beat.notes
 
     def compute_note(self, beat):
-        note=beat.notes[0]
+        note = beat.notes[0]
         string_to_base_note = {6: 4, 5: 9, 4: 2, 3: 7, 2: 11, 1: 4}
         note_mod = 12
         g_string = note.string
@@ -142,6 +251,7 @@ class SongProcessor:
         offset = note.value
         return (base_note + offset) % note_mod
         return 0
+
     def get_measure_list(self, track):
         return track.measures
 
@@ -196,7 +306,6 @@ class SongProcessor:
                     [contents.append(x) for x in rest_acc]  # update accumulator with rest sequence
                     # parse and compute current note
                     note = self.compute_note(beat)
-
 
                     # update accumulator with current note
                     beat_vector = np.zeros(beat_range)
@@ -279,74 +388,8 @@ class CustomSongProcessor(SongProcessor):
     def get_measure_list(self, track):
         return track
 
-
     def get_beat_list(self, measure):
         return measure[0]
-
-    def process_track(self, track, rest_thr, beat_thr):
-        rest_ctr = 0
-        rest_acc = []
-        note_range = 13
-        note_mod = note_range - 1
-        duration_range = 7
-        beat_range = note_range + duration_range
-        contents = []
-        chunks = []
-        for measure in self.get_measure_list(track):
-            for beat in self.get_beat_list(measure):
-                beat_vector = np.zeros(beat_range)
-                duration = self.get_duration(beat)
-
-                if self.is_chord(beat):
-                    # discard tracks with cords
-                    return None
-
-                if self.is_rest(beat):
-                    rest_ctr += 1
-                    # toggle the indexes of the last note (rest) and the corresponding duration
-                    beat_vector[note_mod] = 1
-                    beat_vector[note_range + duration] = 1
-
-                    rest_acc.append(beat_vector)
-                # the beat has a single note
-                elif rest_ctr > rest_thr:  # the new note is from a different sample group
-                    if len(contents) >= beat_thr:  # avoid dumping sequences that are too short
-                        chunks.append(np.array(contents))
-                    # reset accumulators and parse the current note
-                    rest_ctr = 0
-                    rest_acc = []
-                    note = self.compute_note(beat)
-                    # toggle the indexes of the last corresponding note and duration
-                    beat_vector[note] = 1
-                    beat_vector[note_range + duration] = 1
-
-                    contents = [beat_vector]
-
-                elif rest_ctr > 0:  # new note within the same sample group after a sequence of rests
-                    [contents.append(x) for x in rest_acc]  # update accumulator with rest sequence
-                    # parse and compute current note
-                    note = self.compute_note(beat)
-
-
-                    # update accumulator with current note
-                    beat_vector = np.zeros(beat_range)
-                    beat_vector[note] = 1
-                    beat_vector[note_range + duration] = 1
-                    contents.append(beat_vector)
-                    rest_ctr = 0
-                    rest_acc = []
-                else:  # new note with no leading rests
-                    note = self.compute_note(beat)
-                    # toggle the indexes of the last corresponding note and duration
-                    beat_vector[note] = 1
-                    beat_vector[note_range + duration] = 1
-
-                    contents.append(beat_vector)
-
-        # dump the last notes of the file
-        if len(contents) >= beat_thr:
-            chunks.append(np.array(contents))
-        return chunks
 
 
 if __name__ == "__main__":
@@ -368,8 +411,8 @@ if __name__ == "__main__":
     min_beats: int = params["min_beats"]
     test_rate: float = params["test_rate"]
     parser_class_index = 1 if params["parser_class"] == "custom" else 0
-    parser_classes=[SongProcessor,CustomSongProcessor]
-    parser=parser_classes[parser_class_index](
+    parser_classes = [SongProcessor, CustomSongProcessor]
+    parser = parser_classes[parser_class_index](
         input_path,
         output_path,
         silence_thr,
@@ -377,4 +420,4 @@ if __name__ == "__main__":
         track_name,
         test_rate
     )
-    parser.process_songs()
+    parser.get_track_insight()
